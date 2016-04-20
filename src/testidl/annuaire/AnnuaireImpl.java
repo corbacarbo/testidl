cdb280c8641f57f963f6cf7b7d0190c70d65063a
corbacarbo <corbacarbo@gmail.com>
2016-04-20 16:59:58 +0200

    Revue chargement de l'annuaire : Ajout d'un chargeur indépendant.

--- src/testidl/annuaire/AnnuaireImpl.java
+++ src/testidl/annuaire/AnnuaireImpl.java
@@ -11,29 +11,21 @@ import java.util.HashMap;
 public class AnnuaireImpl implements annuaireOperations {
 
   /* Le paramètre String est le matricule de Personne */
-  private final HashMap<String, Personne> annuaire;
+  private HashMap<String, Personne> annuaire;
+  private HashMap<String, String> loginInfo;
 
   public AnnuaireImpl() {
-	super();
-	annuaire = new HashMap<>();
-
-	init();
+    super();
+    remplirAnnuaire();
   }
 
-  /**
-   * Remplit l'annuaire avec des personnes pré-déterminées. Permet de faire des
-   * tests
-   */
-  private void init() {
-	PersonnePermanent p;
-	p = new PersonnePermanent("john", "pjohn", "Nom", "John");
-	annuaire.put(p.getMatricule(), p);
-	p = new PersonnePermanent("james", "pjames", "Nom", "James");
-	annuaire.put(p.getMatricule(), p);
-	System.out.println("Annuaire initialisé:");
-	afficher();
+  private void remplirAnnuaire(){
+    annuaire = DataExample.extractPersonnesFromFile("src/testidl/annuaire/listePersonne.txt");
+    afficher();
+    loginInfo = new HashMap<>();
+    loginInfo.put("accueil", "accueil");
   }
-
+  
   /**
    * Cherche un employé permanent dans l'annuaire.
    *
@@ -41,14 +33,14 @@ public class AnnuaireImpl implements annuaireOperations {
    * @return
    */
   private PersonnePermanent getPersonnePermanent(String matricule)
-		  throws personneInexistanteException {
-	Personne p = annuaire.get(matricule);
-	if (p != null && p.isPermanent()) {
-	  return (PersonnePermanent) p;
-	} else {
-	  throw new personneInexistanteException("Matricule " + matricule
-			  + " introuvable pour un employé permanent");
-	}
+          throws personneInexistanteException {
+    Personne p = annuaire.get(matricule);
+    if (p != null && p.isPermanent()) {
+      return (PersonnePermanent) p;
+    } else {
+      throw new personneInexistanteException("Matricule " + matricule
+              + " introuvable pour un employé permanent");
+    }
   }
 
   /**
@@ -59,75 +51,75 @@ public class AnnuaireImpl implements annuaireOperations {
    * @throws MdpErroneException
    */
   private void mdpEgaux(String mdp1, String mdp2) throws MdpErroneException {
-	if (!mdp1.equals(mdp2)) {
-	  throw new MdpErroneException("Mot de passe erroné");
-	}
+    if (!mdp1.equals(mdp2)) {
+      throw new MdpErroneException("Mot de passe erroné");
+    }
   }
 
   private void afficher() {
-	for (Personne p : annuaire.values()) {
-	  System.out.println(p);
-	}
+    for (Personne p : annuaire.values()) {
+      System.out.println(p);
+    }
   }
 
   /**
-   * Vérification des données d'identification des employés permanents et de 
+   * Vérification des données d'identification des employés permanents et de
    * l'accueil (accueil : A FAIRE).
-   * 
+   *
    * @param matricule d'un employé permanent ou login partagé de l'accueil
    * @param mdp d'un employé permanent ou du login partagé de l'accueil
    * @return une cle pour autoriser l'appel de méthodes protégées
-   * @throws loginIncorrectException 
+   * @throws loginIncorrectException
    */
   @Override
   public cle authentification(String matricule, String mdp) throws loginIncorrectException {
-	Cle cle;
-	PersonnePermanent p;
+    Cle cle;
+    PersonnePermanent p;
 
-	try {
-    // Recherche si le matricule correspond à un employé permanent
-	  p = getPersonnePermanent(matricule);
-    
-	  mdpEgaux(mdp, p.getMdp());
-    
-	} catch (personneInexistanteException ex) {
-	  System.out.println("--Echec d'authentification: personne inexistante - " + matricule
-			  + "::" + mdp);
-	  throw new loginIncorrectException("Matricule introuvable ou ne correspondant pas à un employé permanent.");
-	} catch (MdpErroneException ex) {
-	  System.out.println("--Echec d'authentification: mdp erroné - " + matricule
-			  + "::" + mdp);
-	  throw new loginIncorrectException("Matricule introuvable ou ne correspondant pas à un employé permanent.");
-	}
-	cle = new Cle();
-	System.out.println("++Authentification réussie - " + matricule
-			+ "::" + mdp + " - " + cle);
-	return cle.toCle();
+    try {
+      // Recherche si le matricule correspond à un employé permanent
+      p = getPersonnePermanent(matricule);
+
+      mdpEgaux(mdp, p.getMdp());
+
+    } catch (personneInexistanteException ex) {
+      System.out.println("--Echec d'authentification: personne inexistante - " + matricule
+              + "::" + mdp);
+      throw new loginIncorrectException("Matricule introuvable ou ne correspondant pas à un employé permanent.");
+    } catch (MdpErroneException ex) {
+      System.out.println("--Echec d'authentification: mdp erroné - " + matricule
+              + "::" + mdp);
+      throw new loginIncorrectException("Matricule introuvable ou ne correspondant pas à un employé permanent.");
+    }
+    cle = new Cle();
+    System.out.println("++Authentification réussie - " + matricule
+            + "::" + mdp + " - " + cle);
+    return cle.toCle();
   }
 
   @Override
   public boolean modificationMdp(cle cle, String matricule, String nouveauMdp) throws mdpIdentiqueException {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public personne[] rechercherPersonne(String matricule, String nom, String prenom) throws personneInexistanteException {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public personne validerIdentite(String matricule) throws personneInexistanteException {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public personne ajouterPermanent(personne p) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
   @Override
   public personne ajouterTemporaire(personne p) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
   }
 
 }
