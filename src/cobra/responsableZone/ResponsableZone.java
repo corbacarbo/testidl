c79e204d2862aa46e2978e32085c7330d525547e
MelT <melanietanguy.ch@gmail.com>
2016-06-14 17:38:00 +0200

    suite 1 resp Zone

--- src/cobra/responsableZone/ResponsableZone.java
+++ src/cobra/responsableZone/ResponsableZone.java
@@ -5,16 +5,24 @@
  */
 package cobra.responsableZone;
 
+import cobra.Autorisation;
+import cobra.AutorisationRestreinte;
 import cobra.Cle;
 import cobra.CorbaClient;
+import cobra.Date;
+import cobra.Horaire;
 import cobra.Matricule;
 import cobra.Personne;
 import cobra.PersonnePermanent;
+import controleAcces.HeureHelper;
 import controleAcces.annuaire;
 import controleAcces.annuairePackage.loginIncorrectException;
 import controleAcces.annuairePackage.personneInexistanteException;
+import controleAcces.autorisateur;
+import controleAcces.autorisateurPackage.conflitAutorisationException;
 import controleAcces.personneIdl;
-import java.util.ArrayList;
+import controleAcces.sessionExpireeException;
+import controleAcces.sessionInvalidException;
 import java.util.HashMap;
 
 /**
@@ -22,8 +30,8 @@ import java.util.HashMap;
  * @author Mélanie
  */
 public class ResponsableZone extends CorbaClient implements Runnable{
-private String zone;
-
+  private String zone;
+  private String temporaire="T";
   private ResponsableZoneFrame respZoneFrame;
 
   private Cle cle;
@@ -57,23 +65,42 @@ private String zone;
 
     personneIdl personneIdl = annuaire.validerIdentite(mat);
     respZone = new PersonnePermanent(personneIdl);
+    /** TODO - Ajouter test sur RespZone
+     */
   }
   
   public void rechercherPersonne(String mat,String nom,String prenom) throws personneInexistanteException{
       annuaire annuaire = resolveAnnuaire();
       Matricule m = new Matricule(mat);
       personneIdl[] listePersTrouvees = annuaire.rechercherPersonne(m.toIdl(), nom, prenom);
-      for (personneIdl p : listePersTrouvees){
-          Personne pers = new Personne(p);
-          personnesTrouvees.put(p.matricule+" - "+p.nom+" - "+p.prenom, pers);
-      }
+      personnesTrouvees = Personne.tableToHashMap(listePersTrouvees);
   }
   
+  public void ajouterAutorisationP(String pers, String heureD , String minuteD,String heureF , String minuteF) throws conflitAutorisationException, sessionInvalidException, sessionExpireeException{
+      Personne personneAAutoriser = personnesTrouvees.get(pers);
+      Horaire horaireD = new Horaire(heureD+":"+minuteD);
+      Horaire horaireF = new Horaire(heureF+":"+minuteF);
+      autorisateur autorisateur = resolveAutorisateur(zone);
+      Autorisation auth = new Autorisation(personneAAutoriser.getMatricule(), horaireD, horaireF);
+      autorisateur.ajouterAutorisation(cle.toIdl(), auth.toIdl());
+  }
+  
+    public void ajouterAutorisationT(String pers, String heureD , String minuteD,String heureF , String minuteF, String jourD, String jourF) throws conflitAutorisationException, sessionInvalidException, sessionExpireeException{
+      Personne personneAAutoriser = personnesTrouvees.get(pers);
+      Horaire horaireD = new Horaire(heureD+":"+minuteD);
+      Horaire horaireF = new Horaire(heureF+":"+minuteF);
+      Date dateD = new Date(jourD);
+      Date dateF = new Date(jourF);
+      autorisateur autorisateur = resolveAutorisateur(temporaire);
+      AutorisationRestreinte auth = new AutorisationRestreinte(dateD, dateF,zone, personneAAutoriser.getMatricule(), horaireD, horaireF);
+      autorisateur.ajouterAutorisationRestreinte(cle.toIdl(), auth.toIdlR());
+  }
     
   
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        respZoneFrame = new ResponsableZoneFrame(this);
+        respZoneFrame.setVisible(true);
     }
     
 }
